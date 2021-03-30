dde39637774400dcf1532266be8822ccfb08660f
Prathamesh Sai <prathameshsaisankar@gmail.com>
2021-03-30 13:50:54 +0100

    Added more precuations for Codacy

--- covid-vue-app/test/frontend-test.js
+++ covid-vue-app/test/frontend-test.js
@@ -12,9 +12,13 @@ async function openTheApp() {
         //The driver goes to http://localhost:8080/
         await driver.get("http://127.0.0.1:8080");
         numberOfTestsCompleted = numberOfTestsCompleted + 1;
+        /* eslint-disable no-console */
         console.log("RUNNING TEST STEP " + numberOfTestsCompleted + ": Opening the Application at Port 8080..");
+        /* eslint-enable no-console */
     }catch(error){
+        /* eslint-disable no-console */
         console.log(error);
+        /* eslint-enable no-console */
         numberOfErrors = numberOfErrors + 1;
     }
 }
@@ -26,13 +30,19 @@ async function testTitle() {
           assert.equal("Coronavirus total cases", textValue);
         })
         .catch( (err) => {
+            /* eslint-disable no-console */
             console.log(err);
+            /* eslint-enable no-console */
             numberOfErrors = numberOfErrors + 1;
         });
         numberOfTestsCompleted = numberOfTestsCompleted + 1;
+        /* eslint-disable no-console */
         console.log("RUNNING TEST STEP " + numberOfTestsCompleted  + ": Checking the main title of the application is still valid..");
+        /* eslint-enable no-console */
     }catch(error){
+        /* eslint-disable no-console */
         console.log(error);
+        /* eslint-enable no-console */
         numberOfErrors = numberOfErrors + 1;
     }
 }
@@ -48,7 +58,9 @@ async function testNumbersForActiveCases() {
           assert.notEqual("0", textValue);
         })
         .catch( (err) => {
+            /* eslint-disable no-console */
             console.log(err);
+            /* eslint-enable no-console */
             numberOfErrors = numberOfErrors + 1;
         });
 
@@ -61,7 +73,9 @@ async function testNumbersForActiveCases() {
           assert.notEqual("0", textValue);
         })
         .catch( (err) => {
+            /* eslint-disable no-console */
             console.log(err);
+            /* eslint-enable no-console */
             numberOfErrors = numberOfErrors + 1;
         });
 
@@ -74,14 +88,20 @@ async function testNumbersForActiveCases() {
           assert.notEqual("0", textValue);
         })
         .catch( (err) => {
+            /* eslint-disable no-console */
             console.log(err);
+            /* eslint-enable no-console */
             numberOfErrors = numberOfErrors + 1;
         });
 
         numberOfTestsCompleted = numberOfTestsCompleted + 1;
+        /* eslint-disable no-console */
         console.log("RUNNING TEST STEP " + numberOfTestsCompleted  + ": Checking the Active Cases component's values..");
+        /* eslint-enable no-console */
     }catch(error){
+        /* eslint-disable no-console */
         console.log(error);
+        /* eslint-enable no-console */
         numberOfErrors = numberOfErrors + 1;
     }
 }
@@ -97,7 +117,9 @@ async function testNumbersForClosedCases() {
           assert.notEqual("0", textValue);
         })
         .catch( (err) => {
+            /* eslint-disable no-console */
             console.log(err);
+            /* eslint-enable no-console */
             numberOfErrors = numberOfErrors + 1;
         });
 
@@ -110,7 +132,9 @@ async function testNumbersForClosedCases() {
           assert.notEqual("0", textValue);
         })
         .catch( (err) => {
+            /* eslint-disable no-console */
             console.log(err);
+            /* eslint-enable no-console */
             numberOfErrors = numberOfErrors + 1;
         });
 
@@ -123,14 +147,20 @@ async function testNumbersForClosedCases() {
           assert.notEqual("0", textValue);
         })
         .catch( (err) => {
+            /* eslint-disable no-console */
             console.log(err);
+            /* eslint-enable no-console */
             numberOfErrors = numberOfErrors + 1;
         });
 
         numberOfTestsCompleted = numberOfTestsCompleted + 1;
+        /* eslint-disable no-console */
         console.log("RUNNING TEST STEP " + numberOfTestsCompleted  + ": Checking the Closed Cases component's values..");
+        /* eslint-enable no-console */
     }catch(error){
+        /* eslint-disable no-console */
         console.log(error);
+        /* eslint-enable no-console */
         numberOfErrors = numberOfErrors + 1;
     }
 }
@@ -141,16 +171,22 @@ async function testGetDataButton() {
         //The below function gets the xPath for the "Get Data" button
         await driver.findElement(By.xpath('//*[@id="app"]/div/div[2]/div[2]/div[2]/div/div[2]/div/button')).click();
         numberOfTestsCompleted = numberOfTestsCompleted + 1;
+        /* eslint-disable no-console */
         console.log("RUNNING TEST STEP " + numberOfTestsCompleted  + ": Clicking the `GET DATA` button..");
+        /* eslint-enable no-console */
     }catch(error){
+        /* eslint-disable no-console */
         console.log(error);
+        /* eslint-enable no-console */
         numberOfErrors = numberOfErrors + 1;
     }
 }
 
 async function runAllTests() {
     try{
+        /* eslint-disable no-console */
         console.log("TOTAL NUMBER OF TESTS: " + numberOfTests);
+        /* eslint-enable no-console */
         //Open the App using Firefox, at Port 8080
         await openTheApp();
 
@@ -171,13 +207,19 @@ async function runAllTests() {
 
         //Final message to verify results
         if(numberOfTests == numberOfTestsCompleted){
+            /* eslint-disable no-console */
             console.log("ALL TESTS HAVE PASSED!")
+            /* eslint-enable no-console */
         }
         else{
+            /* eslint-disable no-console */
             console.log(numberOfErrors + " TESTS HAVE FAILED!")
+            /* eslint-enable no-console */
         }
     }catch(error){
+        /* eslint-disable no-console */
         console.log(error);
+        /* eslint-enable no-console */
         numberOfErrors = numberOfErrors + 1;
     }
 }
