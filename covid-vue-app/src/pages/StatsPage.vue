ec3af252629cd90b2b17cf5f507f868deb8f86ea
Emer Murphy <emermurphy@Emers-MacBook-Pro.local>
2021-04-06 13:56:27 +0100

    Extracted BarChart into it's own component

--- covid-vue-app/src/pages/StatsPage.vue
+++ covid-vue-app/src/pages/StatsPage.vue
@@ -1,121 +1,48 @@
 <template lang="html">
   <v-container class="py-12 height_class">
     <h1>This is the Stats page</h1>
-    <svg class="barchart" :width="width + marginLeft / 2" :height="height + marginTop">
-    <g :transform="`translate(${marginLeft / 2}, ${marginTop / 2})`">
-      <g class="x-axis" fill="none" :transform="`translate(0, ${height})`" style="color: #888">
-        <path class="domain" stroke="currentColor" :d="`M0.5,6V0.5H${width}.5V6`"></path>
-        <g
-          class="tick"
-          opacity="1"
-          font-size="10"
-          font-family="sans-serif"
-          text-anchor="middle"
-          v-for="(bar, index) in bars"
-          :key="index"
-          :transform="`translate(${bar.x + bar.width / 2}, 0)`"
-        >
-          <line stroke="currentColor" y2="6"></line>
-          <text fill="currentColor" y="9" dy="0.71em">{{ bar.xLabel }}</text>
-        </g>
-      </g>
-      <g class="y-axis" fill="none" :transform="`translate(0, 0)`" style="color: #888">
-        <path class="domain" stroke="currentColor" :d="`M0.5,${height}.5H0.5V0.5H-6`"></path>
-        <g
-          class="tick"
-          opacity="1"
-          font-size="10"
-          font-family="sans-serif"
-          text-anchor="end"
-          v-for="(tick, index) in yTicks"
-          :key="index"
-          :transform="`translate(0, ${y(tick) + 0.5})`"
-        >
-          <line stroke="currentColor" x2="-6"></line>
-          <text fill="currentColor" x="-9" dy="0.32em">{{ tick }}</text>
-        </g>
-      </g>
-      <g class="bars" fill="none">
-        <rect
-          v-for="(bar, index) in bars"
-          fill="#2196f3"
-          :key="index"
-          :height="bar.height"
-          :width="bar.width"
-          :x="bar.x"
-          :y="bar.y"
-        ></rect>
-      </g>
-    </g>
-  </svg>
+     <BarChart
+      class="chart"
+      :data-set="data"
+      :margin-left="40"
+      :margin-top="40"
+      :tick-count="5"
+      :bar-padding="0.5"
+    />
     <Overview></Overview>
   </v-container>
 </template>
 
+
 <script>
-import { scaleLinear, scaleBand } from "d3-scale";
-export default {
-    name: "BarChart",
-    props: {
-      height: { default: 200 },
-      width: { default: 500 },
-      dataSet: { default: [
-            ["Ireland", 236],
-            ["UK", 435],
-            ["Germany", 287],
-            ["France", 130],
-            ["Portugal", 82],
-            ["Greece", 216],
-            ["Spain", 329],
-            ["Italy", 361],
-          ] },
-      marginLeft: { default: 40 },
-      marginTop: { default: 40 },
-      marginBottom: { default: 40 },
-      marginRight: { default: 40 },
-      tickCount: { default: 5 },
-      barPadding: { default: 0.3 }
+  import BarChart from "@/components/BarChart.vue";
+
+  export default {
+    name: "home",
+    data() {
+      return {
+        data: [
+          ["Ireland", 33],
+          ["UK", 24],
+          ["Italy", 22],
+          ["Germany", 29],
+          ["France", 38],
+          ["Portugal", 21],
+          ["Spain", 25],
+          ["Norway", 30],
+        ],
+      };
     },
-  
-    computed: {
-      yTicks() {
-        return this.y.ticks(this.tickCount);
-      },
-      x() {
-        return scaleBand()
-          .range([0, this.width])
-          .padding(this.barPadding)
-          .domain(this.dataSet.map(e => e[0]));
-      },
-      y() {
-        let values = this.dataSet.map(e => e[1]);
-        return scaleLinear()
-          .range([this.height, 0])
-  
-          .domain([0, Math.max(...values)]);
-      },
-      bars() {
-        let bars = this.dataSet.map(d => {
-          return {
-            xLabel: d[0],
-            x: this.x(d[0]),
-            y: this.y(d[1]),
-            width: this.x.bandwidth(),
-            height: this.height - this.y(d[1])
-          };
-        });
-  
-        return bars;
-      }
-    }
+    components: {
+      BarChart,
+    },
   };
-
-
 </script>
 
-<style lang="css" scoped>
-.height_class {
-  min-height: 100vh;
-}
+<style lang="scss" scoped>
+  .chart {
+    margin: 40px auto 0;
+    display: block;
+  }
 </style>
 
