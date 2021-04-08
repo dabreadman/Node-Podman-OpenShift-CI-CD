aafbcb67be0dac34c7c1687afc4245943efe4995
Prathamesh Sai <prathameshsaisankar@gmail.com>
2021-04-08 21:01:32 +0100

    Version that BUILDS

--- covid-vue-app/src/components/layouts/TheFooter.vue
+++ covid-vue-app/src/components/layouts/TheFooter.vue
@@ -1,45 +1,38 @@
-<template>
-  <!-- Footer -->
-  <mdb-footer color="blue" class="font-small pt-4 mt-4">
-    <mdb-container class="text-left">
-      <mdb-row>
-        <mdb-col sm="6">
-          <h5 class="title" style="color:white;">Covid-19 Tracker</h5>
-          <br />
-          <p style="color:white;">Keep up to date with the latest COVID-19 information.</p>
-          <p style="color:white;">Further information at <a  href="https://www2.hse.ie/coronavirus/" style="color:white;" >The Offical HSE website</a></p>
-        </mdb-col>
-        <mdb-col sm="6">
-          <h5 class="title" style="color:white;">Information about this application</h5>
-          <ul style="color:white;">
-            <li style="color:white;" class="list-unstyled">This application receives COVID-19 information from a <a style="color:white;" href="https://rapidapi.com/KishCom/api/covid-19-coronavirus-statistics">COVID-19 API</a>.</li>
-            <li style="color:white;" class="list-unstyled">For population information it uses a  <a style="color:white;" href="https://rapidapi.com/aldair.sr99/api/world-population">Country Population API</a>.</li>
-            <li style="color:white;" class="list-unstyled">You can find statistical information on the <a style="color:white;" href="\">Home Page</a>.</li>
-            <li style="color:white;" class="list-unstyled">You can find visual information on the <a style="color:white;" href="\stats">Statistics Page</a>.</li>
-          </ul>
-        </mdb-col>
-      </mdb-row>
-    </mdb-container>
-    <div class="footer-copyright text-center py-3">
-      <mdb-container fluid style="color:white;">
-        &copy; 2021 Copyright <a style="color:white;" href="https://github.com/dabreadman/Node-Podman-OpenShift-CI-CD"> Group 39 - Software Engineering  Project </a>
-      </mdb-container>
-    </div>
-  </mdb-footer>
-  <!-- Footer -->
+<template lang="html">
+  <v-footer dark padless>
+    <v-card flat tile class="white black--text text-center my-card flex-grow-1">
+      <v-row>
+        <v-col>
+          <p class="pl-12 ml-6 texts">Group 39 project</p>
+        </v-col>
+        <v-col>
+          <v-btn
+            v-for="link in links"
+            :key="link.id"
+            class="mx-4"
+            icon
+            :href="link.url"
+          >
+            <v-icon size="24px" color="#32325d">{{ link.icon }}</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-footer>
 </template>
 
 <script>
-  import { mdbFooter, mdbContainer, mdbRow, mdbCol } from "mdbvue";
-  export default {
-    name: "TheFooter",
-    components: {
-      mdbFooter,
-      mdbContainer,
-      mdbRow,
-      mdbCol
-    }
-  };
+export default {
+  data: () => ({
+    links: [
+      {
+        id: 1,
+        icon: "mdi-github",
+        url: "https://github.com/dabreadman/Node-Podman-OpenShift-CI-CD",
+      },
+    ],
+  }),
+};
 </script>
 
 <style lang="css" scoped></style>
