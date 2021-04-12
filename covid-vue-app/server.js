d6c52d82f645ea02cfa3818883761765bee1a4b0
Prathamesh Sai <prathameshsaisankar@gmail.com>
2021-04-12 23:18:36 +0100

    Extra issues fixed :)

--- covid-vue-app/server.js
+++ covid-vue-app/server.js
@@ -1,19 +1,14 @@
-var express = require("express");
-var serveStatic = require("serve-static");
-var history = require("connect-history-api-fallback");
-
-var app = express();
+let express = require("express");
+let serveStatic = require("serve-static");
+let history = require("connect-history-api-fallback");
+let app = express();
 app.use(history());
-
 app.use(serveStatic(__dirname + "/dist"));
-
 app.get("/", (req, res) => {
     res.status(200).send("This is the home page!");
   });
-
 app.get("/stats", (req, res) => {
     res.status(200).send("This is the stats page!");
   });
-
-var port = process.env.PORT || 5000;
+let port = process.env.PORT || 5000;
 module.exports = app.listen(port);
